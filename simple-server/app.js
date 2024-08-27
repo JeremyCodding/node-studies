62aae6985a94dafa94834ca003378a42e0e63d02
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-08-27 10:11:04 -0300

    Simple Server: Understanding Response object

--- simple-server/app.js
+++ simple-server/app.js
@@ -5,6 +5,13 @@ const server = http.createServer((req, res) => {
   console.log(req.method);
   console.log(req.headers);
   //   process.exit();
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My Page</title></head>");
+  res.write("<body><h1>Hello World</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
