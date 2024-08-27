ec49e5caeded69a7a6134b10aa63bc6c4f5eee16
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-08-27 10:31:54 -0300

    Simple Server: Saving the user message

--- simple-server/app.js
+++ simple-server/app.js
@@ -1,11 +1,33 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  console.log(req.method);
-  console.log(req.headers);
-  //   process.exit();
+  const url = req.url;
+  const method = req.method;
 
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message' /><button>Submit</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My Page</title></head>");
